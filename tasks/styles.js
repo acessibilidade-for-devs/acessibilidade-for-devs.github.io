a2ad9b3da7abd50cd543cd1f1ae28d2ccb1cff7c
Emanuel Gonçalves <emanuelgdesouza@gmail.com>
2017-03-18 16:14:12 -0300

    Update task and run build

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,16 +1,16 @@
 const gulp = require('gulp')
-const minifyCss = require('gulp-clean-css')
 const sass = require('gulp-sass')
 const gulpif = require('gulp-if')
-const rename = require('gulp-rename')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
 
 module.exports = function (args, reload) {
   gulp.task('sass', function () {
     gulp.src('./src/assets/sass/*.sass')
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulpif(args.production, gulp.dest('./static/css/')))
-      .pipe(gulpif(args.production, minifyCss()))
-      .pipe(gulpif(args.production, rename('all.min.css')))
+      .pipe(gulpif(args.production, postcss([autoprefixer({
+        browsers: ['last 9 versions']
+      })])))
       .pipe(gulp.dest('./static/css/'))
     if (!args.production) reload()
   })
